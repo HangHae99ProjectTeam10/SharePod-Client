2c24e5250cfc0c3e59ffb956073c0343228f9c85
JM <daga4242@gmail.com>
2022-03-17 11:19:17 +0900

    feat: MyPage 세부 컴포넌트 생성

---
+++ src/routes/MyPage/LikeList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LikeList = () => {
+  return <>LikeList</>;
+};
+
+export default LikeList;
---
+++ src/routes/MyPage/MyUserInfo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyUserInfo = () => {
+  return <>MyUserInfo</>;
+};
+
+export default MyUserInfo;
