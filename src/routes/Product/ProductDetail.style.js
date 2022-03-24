940a6ca3579f334eabde5f08d1035edae20a0e1d
JM <daga4242@gmail.com>
2022-03-24 11:44:32 +0900

    feat: 상세페이지-거래요청 연결

--- src/routes/Product/ProductDetail.style.js
+++ src/routes/Product/ProductDetail.style.js
@@ -166,6 +166,7 @@ export const InfoBoxBottom = styled.div`
       font-weight: bold;
       margin-right: 20px;
       box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+      cursor: pointer;
     }
     .info_middle_btn_1 {
       background: ${main_color};
